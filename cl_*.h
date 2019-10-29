956e96e12bd4795af3051eeccda927b35beb0f21
syxeev <56076946+syxeev@users.noreply.github.com>
2019-10-29 09:08:03 +0300

    Create cl_*.h

---
+++ cl_*.h
@@ -0,0 +1,58 @@
+#ifndef CL_1_H
+#define CL_1_H
+
+
+#include "cl_base.h"
+
+class cl_1: public cl_base{
+	public:
+	void get_name_by_addr(string);
+
+	
+};
+
+#endif // CL_3_H
+_______________________
+#ifndef CL_2_H
+#define CL_2_H
+
+
+#include "cl_base.h"
+
+class cl_2: public cl_base
+{
+
+	
+};
+
+#endif // CL_3_H
+---------------------
+#ifndef CL_3_H
+#define CL_3_H
+
+
+#include "cl_base.h"
+
+class cl_3: public cl_base
+{
+public:
+cl_3();
+	
+};
+
+#endif // CL_3_H
+________________
+#ifndef CL_4_H
+#define CL_4_H
+
+
+#include "cl_base.h"
+
+class cl_4: public cl_base
+{
+public:
+cl_4();
+	
+};
+
+#endif // CL_4_H
